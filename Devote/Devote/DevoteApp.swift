42aabed2ec21a92bf202788369dc8e640cf6e0c8
Oleh Didushok <o.didushok@lampalampa.net>
2024-08-03 17:12:31 +0300

    Configured project

---
+++ Devote/Devote/DevoteApp.swift
@@ -0,0 +1,20 @@
+//
+//  DevoteApp.swift
+//  Devote
+//
+//  Created by Oleh on 03.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct DevoteApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
