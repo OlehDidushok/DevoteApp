be5ccfaecad11b4c75326994ac3aeee1af91022f
Oleh Didushok <o.didushok@lampalampa.net>
2024-08-03 17:55:24 +0300

    Configured

---
+++ Devote/Devote/Utility/Constant.swift
@@ -0,0 +1,20 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by Oleh on 03.08.2024.
+//
+
+import SwiftUI
+
+// MARK: - FROMATTER
+private let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: - UI
+
+// MARK: - UX
