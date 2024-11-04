8980b0d418ebbc712bde930313e24bf58fae37e4
Oleh Didushok <didushokov@gmail.com>
2024-11-04 19:20:58 +0200

    Added WidgetAPI

---
+++ Devote/DevoteWidget/DevoteWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  DevoteWidgetBundle.swift
+//  DevoteWidget
+//
+//  Created by Oleh on 04.11.2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DevoteWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DevoteWidget()
+    }
+}
